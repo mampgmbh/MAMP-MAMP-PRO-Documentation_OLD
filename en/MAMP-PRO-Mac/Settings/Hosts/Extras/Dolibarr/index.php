2a045abbed0d7393e0c92bbcde59da4b2e8f4f46
alexpudenz <alexpudenz@users.noreply.github.com>
2017-09-19 16:26:06 +0200

    Update index.php

--- en/MAMP-PRO-Mac/Settings/Hosts/Extras/Dolibarr/index.php
+++ en/MAMP-PRO-Mac/Settings/Hosts/Extras/Dolibarr/index.php
@@ -10,45 +10,32 @@ language: en
 
 Dolibarr ERP & CRM is a modern and easy to use software package to manage your business. More information about Dolibarr can be found [here](https://www.dolibarr.org){:target="_blank"}.
 
-![MAMP](/en/MAMP-PRO-Mac/Settings/Hosts/Extras/Concrete5/concreteExtra.png)
+![MAMP](/en/MAMP-PRO-Mac/Settings/Hosts/Extras/Dolibarr/dolibarrExtra.png)
 
-*  **Name of the site:**  
-   Name your site.
+*  **Name of the company:**  
+   Name your company.
 
 *  **Email address:**  
-   Your email address.  
-   
-   <div class="alert" role="alert"> 
-   Note: You must enter a valid email address to setup a concrete5 installation.
-   </div>
+   Set your superUser email address.  
 
 *  **Directory:**  
-   The installation directory. If it is left blank the concrete5 files will be copied directly to the document root folder.  
-  
-   <div class="alert" role="alert"> 
-   Note: Do not install over a previous installation of concrete5! Files will be overwritten without warning. 
-   </div>
+   The installation directory. If it is left blank the dolibarr files will be copied directly to the document root folder.  
 
 *  **Database name:**  
    Set the name of your database schema which will be added to your local database.  
-   After installation you can view this database for this instance of concrete5 using phpMyAdmin, Sequel Pro, or           MySQLWorkbench. 
+   After installation you can view this database for this instance of dolibarr using phpMyAdmin, Sequel Pro, or           MySQLWorkbench. 
  
 *  **Username:**  
-   The concrete5 admin.
+   Set the dolibarr superUser name.
    <div class="alert" role="alert">
-   Note: You will need this username to login into your new of concrete5 site, please write this down. 
+   Note: You will need this username to login into your new of dolibarr site, please write this down. 
    </div>
 
 *  **Password:**  
-   The concrete5 admins default password.  
+   Set the dolibarr superUser password.  
    <div class="alert" role="alert">   
-   Note: You will need this password to login into your new concrete5 site, please write this down.
+   Note: You will need this password to login into your new dolibarr site, please write this down.
    </div>
    
    ---
 
-### Installation
- 
-After your Concrete5 extra is installed you will see two links to your site. The ![MAMP](/en/MAMP-PRO-Mac/Settings/Hosts/Extras/BlackArrow.png) button will open your Extra's homepage. The ![MAMP](/en/MAMP-PRO-Mac/Settings/Hosts/Extras/gear.png) button will open the admin page of your Concrete5 site. Your admin page is in the `/login` subdirectory of your document root. Your database name is indicated to the right of the  ![MAMP](/en/MAMP-PRO-Mac/Settings/Hosts/Extras/mysql.png) icon.
-
-![MAMP](/en/MAMP-PRO-Mac/Settings/Hosts/Extras/Concrete5/installConcrete.png)
